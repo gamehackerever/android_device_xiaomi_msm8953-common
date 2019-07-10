e47aebfac5082aa03576f36085e52057bcb369ce
zljing <zljing@codeaurora.org>
2019-07-10 02:54:06 +0200

    msm8953-common: boot: shorten wait time to optimize shutdown time
    Mountservice will check if there exist processes occupy files. Launch
    many 3rd apps and set in background will cause many processes occupy
    files, which will cause mountservice spend 10~20s to unmount(sometimes
     also unmount fail with 20s time out). If there is no process occupy
    files, mountservice only need about 500ms. So shorten wait time from
    20s to 500ms, which will save 10~20s for shutdown time, total shutdown
    time is in 3s.
    
    CRs-Fixed: 2038121
    
    Change-Id: I4da7bbf4c86d05add7f1b1e328f70796f3bb8ee8
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- prop.mk
+++ prop.mk
@@ -56,6 +56,10 @@ ro.qualcomm.bluetooth.opp=true \
 ro.qualcomm.bluetooth.pbap=true \
 ro.qualcomm.bt.hci_transport=smd
 
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.vendor.shutdown.waittime=500
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 camera.display.lmax=1280x720 \
